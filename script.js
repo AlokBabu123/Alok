7ddb57e3a187528d0363888c86925b7dfef9374b
unknown <alokbabu7799@gmail.com>
2023-03-13 15:51:44 +0530

    jhdgu

---
+++ script.js
@@ -0,0 +1,85 @@
+// var y=20;
+// {
+//     let x =10;
+//     console.log(x)
+// }
+// console.log(y);
+// const pi=3.14;
+// console.log(pi);
+//var sum = 5+10;
+//console.log(sum);
+//var nul = 5*10;
+//console.log(nul);
+//var x = 10;
+//var y = 5;
+//var sum = x+y;
+//console.log(sum);
+//document.write("sum ="+sum)
+//var first = "Alok";
+//var last = "Babu";
+//document.write(`fullname is ${first} ${last}`);
+//var student={
+  //  name:"alok",
+    //age:21,
+    //place:"thrissur"
+//}
+//console.log(student);
+//console.log(student.name);
+//var fruits =["apple","banana","orange",10];
+//fruits[2]="grapes"
+//fruits.push("plum");
+//fruits.unshift("kiwi")
+//fruits.pop()
+//fruits.shift()
+//console.log(fruits);
+//var person =[
+  //  {
+    //    name:"Tiya",
+      //  age:10,
+        //place:"Koratty"
+    //},
+    //{
+      //  name:"Ajas",
+        //age:20,
+       // place:"Aluva"
+    //}
+//]
+//console.log(person[1].name);
+//function add(a,b){
+  ///  var c = a*b;
+  //  console.log(c)
+////}
+//add(1,2);
+//add(2,3);
+
+//const addition =(a,b)=>{
+  //  var c = a+b;
+    //console.log(c)
+
+//}
+///addition(1,2)
+//var x= 20;
+//if(x>20){
+  //  console.log("good");
+//}
+//else
+//console.log("bad");
+//for(var i=1;i<=5;i++)
+//{
+  //  console.log(i);
+//}
+var fruits=["apple","orange","mango","grape"];
+//for(let i=0;i<fruits.length;i++)
+//{
+  //  console.log(fruits[i]);
+//}
+//let p=0;
+//while(p<fruits.length)
+//{console.log(fruits[p]);
+  //  p++;
+//}
+let i=0;
+do{
+    console.log(fruits[i]);
+    i++
+}while(i<fruits.length)
